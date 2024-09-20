dce4537a56d9a29a73f24ddf0d44425c5b9c77b6
Artlz21 <darren.lozano01@utrgv.edu>
2024-09-19 23:14:56 -0500

    Set up for the FlashcardStacks is almost done, need to finish each of the other tables.

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -1,4 +1,5 @@
 using System.Data.SqlClient;
+using System.Globalization;
 
 namespace FlashcardApp {
     public class DatabaseManager {
@@ -14,48 +15,94 @@ namespace FlashcardApp {
                 Console.WriteLine("DB Connected successfully");
         }
 
-        public void FlashcardStackGetAll() {
+        public List<FlashcardStack>? FlashcardStackGetAll() {
+            List<FlashcardStack> FlashcardStackList = new();
+            
             using var connection = new SqlConnection(ConnectionString);
-                connection.Open();
                 string query = "SELECT * FROM FlashcardStacks";
+                connection.Open();
 
                 using var cmd = new SqlCommand(query, connection);
                     using SqlDataReader reader = cmd.ExecuteReader();
                         while (reader.Read())
                         {
-                            Console.WriteLine($"ID: {reader["ID"]}, Stack Name: {reader["StackName"]}");
+                            FlashcardStack flashcardStack = new();
+                            flashcardStack.Id = (int)reader["ID"];
+                            flashcardStack.Name = (string)reader["StackName"];     
+                            FlashcardStackList.Add(flashcardStack);                  
                         }
+            
+            return FlashcardStackList;
         }
 
-        public void FlashcardStackGet(string stack) {
+        public FlashcardStack? FlashcardStackGet(string stack) {
+            FlashcardStack flashcardStack = new();
+
             using var connection = new SqlConnection(ConnectionString);
-            connection.Open();
-                using var cmd = new SqlCommand();
-                    cmd.Connection = connection;
-                    cmd.CommandText = 
-                    """
-                    SELECT * FROM FlashcardStacks
-                    WHERE StackName = @StackName
-                    """;
-                
+                string query = 
+                """
+                SELECT * FROM FlashcardStacks
+                WHERE StackName = @StackName
+                """;
+                connection.Open();
+
+                using var cmd = new SqlCommand(query, connection);                
                     cmd.Parameters.Add(new SqlParameter("@StackName", stack));
 
                     using SqlDataReader reader = cmd.ExecuteReader();
                         while (reader.Read()) {
-                            Console.WriteLine($"ID: {reader["ID"]}, Stack Name: {reader["StackName"]}");
+                            flashcardStack.Id = (int)reader["ID"];
+                            flashcardStack.Name = (string)reader["StackName"];
                         }
+            
+            return flashcardStack;
         }
 
-        public void FlashcardStackPost() {
+        public void FlashcardStackPost(FlashcardStack flashcardStack) {
+            using var connection = new SqlConnection(ConnectionString);
+                string query = 
+                """
+                INSERT INTO FlashcardStacks (StackName)
+                Values (@StackName)
+                """;
+                connection.Open();
 
+                using var cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.Add(new SqlParameter("@StackName", flashcardStack.Name));
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("New stack added.");
         }
 
-        public void FlashcardStackDelete() {
+        public void FlashcardStackDelete(FlashcardStack flashcardStack) {
+            using var connection = new SqlConnection(ConnectionString);
+                string query = 
+                """
+                DELETE FROM FlashcardStacks 
+                WHERE StackName = @StackName
+                """;
+                connection.Open();
 
+                using var cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.Add(new SqlParameter("@StackName", flashcardStack.Name));
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Stack deleted.");
         }
 
-        public void FlashcardStackUpdate() {
+        public void FlashcardStackUpdate(FlashcardStack flashcardStack) {
+            using var connection = new SqlConnection(ConnectionString);
+                string query = 
+                """
+                UPDATE FlashcardStacks 
+                SET StackName = @StackName
+                WHERE ID = @ID
+                """;
+                connection.Open();
 
+                using var cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.Add(new SqlParameter("@StackName", flashcardStack.Name));
+                    cmd.Parameters.Add(new SqlParameter("@ID", flashcardStack.Id));
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Stack updated");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ class Program
     static void Main(string[] args)
     {
         DatabaseManager databaseManager = new("Server=(localdb)\\FlashcardDB;Database=FlashcardDB;Trusted_Connection=True;");
-        databaseManager.FlashcardStackGet("Second");
+        List<FlashcardStack>? list = databaseManager.FlashcardStackGetAll();
+        foreach(var l in list) {
+            Console.WriteLine(l.Name);
+        }
 
         // Menu menu= new Menu();
 
