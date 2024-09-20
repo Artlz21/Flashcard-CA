5f8420cd8057eb139753475d2a82d635a266fdde
Artlz21 <darren.lozano01@utrgv.edu>
2024-09-19 23:41:24 -0500

    Set up for the FlashcardStacks is almost done, need to finish each of the other tables.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,8 @@ class Program
     {
         DatabaseManager databaseManager = new("Server=(localdb)\\FlashcardDB;Database=FlashcardDB;Trusted_Connection=True;");
         List<FlashcardStack>? list = databaseManager.FlashcardStackGetAll();
-        foreach(var l in list) {
-            Console.WriteLine(l.Name);
-        }
 
-        // Menu menu= new Menu();
+        // Menu menu = new Menu();
 
         // menu.StartApp();
     }
